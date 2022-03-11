c3565dab78bdf439c6d2f88b9f90c15480805723
minTwin <michaelmin2020@gmail.com>
2022-03-11 08:28:48 -0600

    implemented bcrypt and salting to add another layer of security when hashing passwords

--- Secrets_Starting_Code/app.js
+++ Secrets_Starting_Code/app.js
@@ -4,12 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -26,7 +24,6 @@ const userSchema = new mongoose.Schema ({
 
 
 const secret = "Thisisourlittlesecret.";
-//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,24 +40,30 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });    
+            newUser.save(function(err){
+                if(err) {
+                    console.log(err);
+                }
+                else {
+                    res.render("secrets");
+                }
+            });
+        });
     });
 
-    newUser.save(function(err){
-        if(err) {
-            console.log(err);
-        }
-        else {
-            res.render("secrets");
-        }
-    });
+
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
@@ -68,8 +71,12 @@ app.post("/login", function(req, res){
         }
         else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
+                if (foundUser) {
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        if (result === true) {
+                            res.render("secrets");                            
+                        }
+                    });   
                 }
             }
         }
